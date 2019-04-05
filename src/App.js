757872fb40f43d4ff9eb98dcf8b4b008bb2803d3
alfieqashwa <alfieqashwa@gmail.com>
2019-04-06 02:28:10 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// Custom Hooks
+const useHackerNewsApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+  fetchData();
+  }, [url]);
+
+  const doFetch = url => {
+    setUrl(url);
+  };
+  return { data, isLoading, isError, doFetch };
+}
+
+// Function Component
+function App() {
+  const [query, setQuery] = useState('redux');
+  const { data, isLoading, isError, doFetch } = useHackerNewsApi(
+    'http://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] },
+  );
+
+
+  return (
+    <Fragment>
+      <form onSubmit={event => {
+        doFetch(
+          `http://hn.algolia.com/api/v1/search?query=${query}`,
+        );
+
+          event.preventDefault();
+        }}>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}></a>
+            </li>
+          ))}
+        </ul>
+      )}
+  </Fragment>
+  );
 }
 
 export default App;
